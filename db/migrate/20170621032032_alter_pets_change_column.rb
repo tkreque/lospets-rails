4998f3983f3269cd36754d1385ab01df5ba270d4
Thiago Klein Reque <blindreque@gmail.com>
2017-06-21 03:29:47 +0000

    new file:   db/migrate/20170621032032_alter_pets_change_column.rb 	modified:   db/schema.rb

---
+++ db/migrate/20170621032032_alter_pets_change_column.rb
@@ -0,0 +1,7 @@
+class AlterPetsChangeColumn < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :pets, :animal_id, :integer
+    add_column :pets, :race_id, :integer
+    add_column :pets, :situation, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611180802) do
+ActiveRecord::Schema.define(version: 20170621032032) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "animal"
