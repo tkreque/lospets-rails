07824816f9381bf3328a71dd9138474318779e56
Thiago Klein Reque <blindreque@gmail.com>
2017-06-21 03:49:19 +0000

    Heroku 05

--- db/migrate/20170621032032_alter_pets_change_column.rb
+++ db/migrate/20170621032032_alter_pets_change_column.rb
@@ -1,6 +1,8 @@
 class AlterPetsChangeColumn < ActiveRecord::Migration[5.0]
   def change
     remove_column :pets, :animal_id, :integer
+    remove_column :pets, :longitue, :float
+    add_column :pets, :longitude, :float
     add_column :pets, :race_id, :integer
     add_column :pets, :situation_id, :integer
   end
